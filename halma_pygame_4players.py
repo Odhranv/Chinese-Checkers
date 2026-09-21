d081b02edc97fadc6443095d149900bc8530480a
Michael Herlea <mihaijherlea+github@gmail.com>
2026-09-21 16:21:52 +0200

    Enable tree vizualisation

--- halma_pygame_4players.py
+++ halma_pygame_4players.py
@@ -27,7 +27,7 @@ from halma import (
 from AI_Player_Team12 import AI_Player_Team12
 
 MAXIMUM_MOVE_LIMIT: int = 100  # Total turns across all four players, including skipped turns.
-VISUALIZE_SEARCH_TREE: bool = False # This is here to match the function signature, the random bot does not visualize anything 
+VISUALIZE_SEARCH_TREE: bool = True # This is here to match the function signature, the random bot does not visualize anything
 BOT_DELAY_MS: int = 450
 
 INITIAL_BOARD: List[List[int]] = initial_pos
